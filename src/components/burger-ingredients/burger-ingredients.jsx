208753afa7776077cffe3fe58f31fca911e8bb5c
Mishe4ka14 <mishechka.14@yandex.ru>
2023-07-02 05:08:07 +0300

    add handle scroll

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -2,13 +2,9 @@ import styles from './burger-ingredients.module.css'
 import { Tab, } from '@ya.praktikum/react-developer-burger-ui-components'
 import React from 'react'
 import IngredientList from '../ingredient-list/ingredient-list'
-import PropTypes from 'prop-types';
-import { ConstructorContext } from '../../services/constructor-context'
-import { useContext, useEffect } from 'react'
-import { useState } from 'react';
+import { useEffect } from 'react'
 import { getIngredients } from '../../services/actions/ingredients';
 import { useDispatch, useSelector } from 'react-redux';
-
 
 const BurgerIngredients = () => { 
 
@@ -19,31 +15,58 @@ const BurgerIngredients = () => {
   }, [])
   
   const {ingredient} = useSelector(store => store.ingredient)
-  const [current, setCurrent] = React.useState('bun')
   const data = ingredient;
   const buns = data.filter((ingredient) => ingredient.type === 'bun');
   const sauces = data.filter((ingredient) => ingredient.type === 'sauce');
   const mains = data.filter((ingredient) => ingredient.type === 'main');
+  
+  const [activeTab, setActiveTab] = React.useState('bun');
+
+    useEffect(() => {
+      const handleScroll = () => {
+        const sections = ['.bun', '.sauce', '.main'];
+    
+        const distances = sections.map((section) => {
+          const element = document.querySelector(section);
+          const distance = Math.abs(element.getBoundingClientRect().top - 90);
+          return { section, distance };
+        });
+    
+        const closestSection = distances.reduce((closest, current) =>
+          current.distance < closest.distance ? current : closest
+        );
+    
+        setActiveTab(closestSection.section.slice(1));
+      };
+    
+      const scrollWrapper = document.querySelector('.custom-scroll');
+      scrollWrapper.addEventListener('scroll', handleScroll, { passive: true });
+    
+      return () => {
+        scrollWrapper.removeEventListener('scroll', handleScroll);
+      };
+    }, []);
+
   return(
     <section className={styles.section}>
       <h2 className='text text_type_main-large mt-10 mb-5'>Соберите бургер</h2>
       <div style={{ display: 'flex' }}>
-        <Tab value="bun" active={current === 'bun'} onClick={setCurrent}>
+        <Tab value="bun" active={activeTab === 'bun'} onClick={() => setActiveTab('bun')}>
           Булки
         </Tab>
-        <Tab value="sauce" active={current === 'sauce'} onClick={setCurrent}>
+        <Tab value="sauce" active={activeTab === 'sauce'} onClick={() => setActiveTab('sauce')}>
           Соусы
         </Tab>
-        <Tab value="main" active={current === 'main'} onClick={setCurrent}>
+        <Tab value="main" active={activeTab === 'main'} onClick={() => setActiveTab('main')}>
           Начинки
         </Tab>
       </div>
       <ul className={`${styles.scroll} custom-scroll`}>
-          <h3 className="text text_type_main-medium mb-6">Булки</h3>
+          <h3 className="bun text text_type_main-medium mb-6">Булки</h3>
           <IngredientList filter={buns} type="bun"/>
-          <h3 className="text text_type_main-medium mt-10 mb-6">Соусы</h3>
+          <h3 className="sauce text text_type_main-medium mt-10 mb-6">Соусы</h3>
           <IngredientList filter={sauces} type="sauce"/>
-          <h3 className="text text_type_main-medium mt-10 mb-6">Начинки</h3>
+          <h3 className="main text text_type_main-medium mt-10 mb-6">Начинки</h3>
           <IngredientList filter={mains} type="main"/>
       </ul>
     </section>

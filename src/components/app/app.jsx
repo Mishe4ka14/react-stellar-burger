3dc4c727403d98dd84e7bd0fb89c28de787cb203
Mishe4ka14 <mishechka.14@yandex.ru>
2023-08-10 21:38:59 +0300

    add react-router and homePage

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,21 +1,14 @@
-import styles from "./app.module.css";
-import AppHeader from "../app-header/app-header";
-import BurgerIngredients from "../burger-ingredients/burger-ingredients";
-import BurgerConstructor from "../burger-constructor/burger-constructor";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Routes } from 'react-router';
+import HomePage from "../../pages/home-page";
 
 function App() {
   return (
-    <div className={styles.app}>
-      <AppHeader/>
-      <main className={styles.main}>
-        <DndProvider backend={HTML5Backend}>
-              <BurgerIngredients/>
-              <BurgerConstructor/>
-        </DndProvider>    
-      </main>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import { Provider, createStoreHook } from "react-redux";
 import { rootReducer } from "./services/reducers";
 import { compose, createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Routes } from 'react-router';
+import HomePage from "./pages/home-page";
 
 const composeEnhancers =
 typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
@@ -21,7 +24,7 @@ const store = createStore(rootReducer, enhancer)
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+        <HomePage/>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
